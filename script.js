de20088bf3cffe080df81e28a4b7c49bf837aec5
FranSar5868 <fsarrias@coit.es>
2023-05-10 17:02:17 +0200

    I create the URL when clicking

--- script.js
+++ script.js
@@ -30,13 +30,22 @@ async function fetchRandomPokemon() {
         let img = clonedTemplate.querySelector('img'); // seleccionar la img i guardar-la en una variable
         // let label = clonedTemplate.querySelector('label'); // seleccionar la label i guardar-la una variable
         // let check = clonedTemplate.querySelector('input'); // seleccionar el checkbox i guardar-lo en una variable
+        let button = clonedTemplate.querySelector('input[type="submit"]'); // select the Detalls button
 
         card.setAttribute('id', pokemon.id); // assigna l'id rebut de la API
         randomFact.textContent = pokemon.name; // assigna el text rebut de la API
         img.setAttribute('src', pokemon.sprites.front_default);
         // label.setAttribute('for', `check-${pokemon.id}`); // assigna a l'atribut for l'id rebut de la API amb el prefix "check-", per enllaçar-lo al checkbox
         // check.setAttribute('id', `check-${pokemon.id}`); // assigna l'id rebut de la API amb el prefix "check-", per evitar duplicat amb l'id de l'article
-        // button.setAttribute('data-pokemon-id', pokemon.id); // store the Pokemon ID as a data attribute
+        button.setAttribute('data-pokemon-id', pokemon.id); // add a data attribute to the Details button
+
+        button.addEventListener('click', (event) => {
+          const pokemonId = event.target.dataset.pokemonId; // retrieve the Pokemon ID from the clicked button's data attribute
+          // navigate to the detail page with the Pokemon ID as a query parameter
+          const baseURL = window.location.origin;
+          const new_url = `${baseURL}?PokeId=${pokemonId}`;
+          window.location.href = new_url; // Navigate to the URL
+        });
 
         out.appendChild(clonedTemplate); // afegeix el clon amb tota la informació al section id="cards"
 
@@ -45,7 +54,15 @@ async function fetchRandomPokemon() {
     })
     .catch(error => console.error(error));
  
+    document.addEventListener('DOMContentLoaded', () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const pokeId = urlParams.get('PokeId');
     
+      if (pokeId) {
+        // Do something with the pokeId parameter, e.g. fetch the Pokemon data and display it on the page
+        console.dir(promises);
+      }
+    });
  
 
 //     container.addEventListener("click", event => {
